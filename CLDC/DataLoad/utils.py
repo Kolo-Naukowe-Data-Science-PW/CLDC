7a53f089f05fddf96bef785092be41d845757b19
jakwisn <jakwisn@gmail.com>
2020-12-22 09:33:53 +0100

    initial structure

---
+++ CLDC/DataLoad/utils.py
@@ -0,0 +1,4 @@
+def load_data_from_kaggle():
+    # @TODO implement function that import data from kaggle, unzips it, and stores it in data folder
+    # You can consult with your friends what the formats/folder structure should be.
+    pass
---
+++ CLDC/__init__.py
@@ -0,0 +1,5 @@
+from .DataLoad.utils import load_data_from_kaggle
+
+__all__ = [
+    "load_data_from_kaggle"
+]
